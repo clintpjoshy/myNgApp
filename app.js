b133df79e6f03ffd9098f6d398a11e5a96a44c32
Clint Joshy <c-clint.joshy@charter.com>
2017-10-06 15:33:47 -0400

    Some more changes

--- app.js
+++ app.js
@@ -2,6 +2,6 @@
   'use strict';
   var myApp = angular.module("myApp", []);
   myApp.controller('FirstController', ['$scope', function($scope) {
-    $scope.tabs = ['Home', 'About Me', 'Interests', 'Technical Knowledge', 'Current Study', 'My Dream', 'Suhas'];
+    $scope.tabs = ['Home', 'About Me', 'Interests', 'Technical Knowledge', 'Current Study', 'My Dream'];
   }])
 })(window.angular);
