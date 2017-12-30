a61eaaf916aa2948a63d5dcf12834a24b07bd79a
Clint Joshy <c-clint.joshy@charter.com>
2017-12-30 13:12:22 -0500

    some more changes

--- app.js
+++ app.js
@@ -1,7 +1,47 @@
 (function(angular) {
   'use strict';
-  var myApp = angular.module("myApp", []);
+  var myApp = angular.module("myApp", ["ngRoute"]);
+
   myApp.controller('FirstController', ['$scope', function($scope) {
-    $scope.tabs = ['Home', 'About Me', 'Interests', 'Technical Knowledge', 'Current Study', 'My Dream'];
+    $scope.tabs = [
+      {name: 'Home', path: 'home'},
+      {name: 'About Me', path: 'aboutMe'},
+      {name: 'Interests', path: 'interests'},
+      {name: 'Technical Knowledge', path: 'technicalKnowledge'},
+      {name: 'Current Study', path: 'currentStudy'},
+      {name: 'My Dream', path: 'myDream'}
+    ];
   }])
+
+  myApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+
+    .when("/home", {
+      templateUrl: "/src/home/home.html"
+    })
+
+    .when("/aboutMe", {
+      templateUrl: "/src/aboutMe/aboutMe.html"
+    })
+
+    .when("/interests", {
+      templateUrl: "/src/interests/interests.html"
+    })
+
+    .when("/technicalKnowledge", {
+      templateUrl: "/src/technicalKnowledge/technicalKnowledge.html"
+    })
+
+    .when("/currentStudy", {
+      templateUrl: "/src/currentStudy/currentStudy.html"
+    })
+
+    .when("/myDream", {
+      templateUrl: "/src/myDream/myDream.html"
+    })
+
+    .otherwise({
+      template: '<div>Not Found!!</div>'
+    })
+  }]);
 })(window.angular);
